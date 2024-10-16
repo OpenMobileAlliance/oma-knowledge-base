57e49c37aaf95e2c5e725a48a90d3ca6b60ecb11
Filip Dragović <dr.ipi95@gmail.com>
2024-10-16 16:41:53 +0200

    feat: new constructor

---
+++ components/content/sh-img-container.vue
@@ -0,0 +1,33 @@
+<template>
+    <div :class="ui.wrapper">
+        <figure :class="ui.figure">
+            <img :src="urlImage" :alt="altImage" :class="ui.image">
+            <figcaption :class="ui.figcaption">
+                    <MDC :value="figcaption" />
+            </figcaption>
+        </figure>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { imgContainer as config } from '@/ui.config' // Importing the config file
+
+const props = withDefaults(
+    defineProps<{
+        description?: string;
+        urlImage: string;
+        altImage: string;
+        figcaption: string;
+        ui?: Partial<typeof config>;
+    }>(),
+    {
+        ui: () => ({}),
+        description: "",
+    });
+
+const { ui } = useUI(
+    "sh-img-container",
+    toRef(props, "ui"),
+    config
+);
+</script>
---
+++ ui.config/sh-img-container.ts
@@ -0,0 +1,6 @@
+export default{
+    wrapper: 'dark:bg-black pt-3 pb-3 mx-32',
+    figure: 'dark:invert flex flex-col justify-center items-center m-4',
+    image: '',
+    figcaption: 'dark:text-neutral-600',
+}
