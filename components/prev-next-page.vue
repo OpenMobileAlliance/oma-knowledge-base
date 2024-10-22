5d99bea085ef44101c8a0c10b7c3cd3eae4dcb84
Filip Dragović <dr.ipi95@gmail.com>
2024-10-22 19:29:36 +0200

    update:style update

--- components/prev-next-page.vue
+++ components/prev-next-page.vue
@@ -1,22 +1,23 @@
 <template>
-  <hr />
+  <hr class="border-2 dark:border-golden rounded-r-xl rounded-l-xl" />
   <div class="grid grid-cols-2 mt-10 mx-auto max-w-max min-w-full not-prose">
-    <NuxtLink v-if="prev" :to="prev._path" :class="['justify-items-start lg:mr-20 sm:mr-5', ui.button]">
+    <NuxtLink external v-if="prev" :to="prev._path" :class="['justify-items-start lg:mr-20 sm:mr-5', ui.button]">
       <UIcon name="i-vaadin:chevron-circle-left-o" dynamic :class="ui.icon" />
       <div :class="ui.title">{{ prev.title }}</div>
-      <div v-if="prev.description" :class="ui.description">{{ prev.description }}</div>
+      <div v-if="showDescription && prev.description" :class="ui.description">{{ prev.description }}</div>
     </NuxtLink>
-    <NuxtLink v-if="next" :to="next._path" :class="['justify-items-end lg:ml-20 sm:ml-5', ui.button]">
+    <NuxtLink external v-if="next" :to="next._path" :class="['justify-items-end lg:ml-20 sm:ml-5', ui.button]">
       <UIcon name="i-vaadin:chevron-circle-right-o" dynamic :class="ui.icon" />
       <div :class="ui.title">{{ next.title }}</div>
-      <div v-if="next.description" :class="ui.description">{{ next.description }}</div>
+      <div v-if="showDescription && next.description" :class="ui.description">{{ next.description }}</div>
     </NuxtLink>
+    {{ next }}
   </div>
 </template>
 
 <script setup lang="ts">
-const route = useRoute()
-const [prev, next] = await queryContent().findSurround(route.path)
+const route = useRoute();
+const [prev, next] = await queryContent().findSurround(route.path);
 
 const config = {
   button: 'grid grid-rows-3 border p-4 rounded-xl hover:border hover:border-primary/[0.6] dark:hover:border-primary/[0.8] hover:bg-primary/[0.3] dark:hover:bg-primary/[0.2] transition-colors',
@@ -25,13 +26,16 @@ const config = {
   description: 'text-lg font-normal mt-2 text-gray-700 hover:text-primary dark:text-golden/[0.4] dark:hover:text-primary',
 };
 
+// Use defineProps to accept a boolean prop for showDescription
 const props = withDefaults(
   defineProps<{
     ui?: Partial<typeof config>;
+    showDescription?: boolean; // Accept showDescription as a prop
   }>(),
   {
     ui: () => ({}),
-  },
+    showDescription: true, // Default value if not provided
+  }
 );
 
 const { ui } = useUI(
