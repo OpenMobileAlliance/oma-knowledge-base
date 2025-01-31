1f97ca717e499cc0b36a7981d04cfe22ff1cb372
Filip Dragović <dr.ipi95@gmail.com>
2025-01-31 17:06:41 +0100

    update: folder w/ only one file is clickable, else it expands

--- components/app/menus.vue
+++ components/app/menus.vue
@@ -3,7 +3,8 @@
         <li v-for="(item, index) in menuData.items" :key="index" class="group/item relative">
             <!-- Root Menu Button -->
             <button @click="item.onClick"
-                class="w-full flex items-center justify-start px-3 py-2 text-left hover:bg-white dark:hover:bg-gray-800 hover:rounded-t-lg">
+                class="w-full flex items-center justify-start px-3 py-2 text-left hover:bg-white dark:hover:bg-gray-800"
+                :class="[item.label === 'HOME' ? 'hover:rounded-lg cursor-pointer' : 'hover:rounded-t-lg cursor-default']">
                 <span class="flex items-center space-x-2">
                     <!-- <UIcon v-if="frontmatter[0].icon" :name="frontmatter[0].icon" dynamic /> -->
                     <span class="font-medium text-xl text-gray-900 dark:text-gray-100">
@@ -15,10 +16,11 @@
 
             <!-- Submenu -->
             <ul v-if="item.children"
-                class="absolute top-10 left-0 hidden group-hover/item:flex flex-col shadow-lg rounded-b-lg rounded-tr-lg hover:rounded-tl-lg mt-0 bg-inherit bg-white dark:bg-gray-800 z-50">
+                class="absolute top-10 left-0 hidden group-hover/item:flex flex-col shadow-lg rounded-b-lg rounded-tl-none rounded-tr-lg hover:rounded-tl-lg mt-0 bg-inherit bg-white dark:bg-gray-800 z-50">
                 <li v-for="(child, childIndex) in item.children" :key="childIndex" class="group/sub relative">
                     <button @click="child.onClick"
-                        class="w-full space-x-2 flex items-center px-4 py-2 text-left hover:rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600 transition ease-out duration-200">
+                        class="w-full space-x-2 flex items-center px-4 py-2 text-left hover:rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600 transition ease-out duration-200"
+                        :class="[child.children ? 'cursor-default' : 'cursor-pointer']">
                         <!-- <UIcon v-if="frontmatter[0].icon" :name="frontmatter[0].icon" dynamic /> -->
                         <span class="font-small text-gray-900 dark:text-gray-100 truncate">
                             {{ child.label }}
@@ -32,7 +34,8 @@
                         <li v-for="(subChild, subChildIndex) in child.children" :key="subChildIndex"
                             class="group/subsub relative">
                             <button @click="subChild.onClick"
-                                class="w-full space-x-2 flex items-center px-4 py-2 text-left hover:rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600 transition duration-200">
+                                class="w-full space-x-2 flex items-center px-4 py-2 text-left hover:rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600 transition duration-200"
+                                :class="[subChild.children ? 'cursor-default' : 'cursor-pointer']">
                                 <!-- <UIcon v-if="frontmatter[0].icon" :name="frontmatter[0].icon" dynamic /> -->
                                 <span class="font-medium text-gray-900 dark:text-gray-100 truncate">
                                     {{ subChild.label }}
@@ -46,7 +49,8 @@
                                 <li v-for="(deepChild, deepChildIndex) in subChild.children" :key="deepChildIndex"
                                     class="group/deeper relative">
                                     <button @click="deepChild.onClick"
-                                        class="w-full space-x-2 flex items-center px-4 py-2 text-left hover:rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600 transition duration-200">
+                                        class="w-full space-x-2 flex items-center px-4 py-2 text-left hover:rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600 transition duration-200"
+                                        :class="[deepChild.children ? 'cursor-default' : 'cursor-pointer']">
                                         <!-- <UIcon v-if="frontmatter[0].icon" :name="frontmatter[0].icon" dynamic /> -->
                                         <span class="font-medium text-gray-900 dark:text-gray-100 truncate">
                                             {{ deepChild.label }}
@@ -60,7 +64,8 @@
                                         <li v-for="(fifthChild, fifthChildIndex) in deepChild.children"
                                             :key="fifthChildIndex" class="relative">
                                             <button @click="fifthChild.onClick"
-                                                class="w-full space-x-2 flex items-center px-4 py-2 text-left hover:rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600 transition duration-200">
+                                                class="w-full space-x-2 flex items-center px-4 py-2 text-left hover:rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600 transition duration-200"
+                                                :class="[fifthChild.children ? 'cursor-default' : 'cursor-pointer']">
                                                 <!-- <UIcon v-if="frontmatter[0].icon" :name="frontmatter[0].icon" dynamic /> -->
                                                 <span class="font-medium text-gray-900 dark:text-gray-100 truncate">
                                                     {{ fifthChild.label }}
@@ -81,20 +86,15 @@
 </template>
 
 <script setup lang="ts">
-//import { MenuBar } from '@imengyu/vue3-context-menu';
-//import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css';
-//import type { MenuBarOptions, MenuItem } from '@imengyu/vue3-context-menu';
-
 const router = useRouter();
 const route = useRoute();
 const { data: navigation } = await useAsyncData('navigation', () => fetchContentNavigation());
 
 const frontmatter = ref<any[]>([])
 
- watchEffect(async () => {
-   frontmatter.value = await queryContent(route.path).where({ icon: { $exists: true } }).find();
-//   console.log(frontmatter.value[0].icon);
- });
+watchEffect(async () => {
+    frontmatter.value = await queryContent(route.path).where({ icon: { $exists: true } }).find();
+});
 
 // List of folder and file titles to filter out
 const excludedTitles = [
@@ -110,33 +110,42 @@ const excludedTitles = [
     'Open Mobile Alliance',
 ];
 
-const excludedPaths = [
-    '',
-];
+const excludedPaths = ['/media/articles'];
+
+const filterExcludedPaths = (navItem: any): boolean => {
+    if (excludedPaths.includes(navItem._path)) {
+        return false;
+    }
+    if (navItem.children) {
+        navItem.children = navItem.children.filter(filterExcludedPaths);
+    }
+    return true;
+};
 
 const filteredNavigation = computed(() =>
     navigation.value?.filter((navItem: any) =>
-        !excludedTitles.includes(navItem.title) && !excludedPaths.includes(navItem._path)
+        filterExcludedPaths(navItem) && !excludedTitles.includes(navItem.title)
     )
 );
-//console.log(filteredNavigation.value);
 
-// Recursive function to process deeper levels of navigation
-const processNavigationItem = (navItem: any): MenuItem => ({
-    label: navItem.title,
-    children: navItem.children
-        ? navItem.children
-            .map((child: any) => processNavigationItem(child))
-        : null,
-    onClick: () => {
-        router.push(navItem._path);
-    },
-});
 
-// Build the full dynamic menu based on filtered navigation
+const processNavigationItem = (navItem: any): MenuItem => {
+    if (navItem.children?.length === 1) {
+        const singleChild = navItem.children[0];
+        return {
+            label: navItem.title,
+            onClick: () => router.push(singleChild._path),
+        };
+    }
+    return {
+        label: navItem.title,
+        children: navItem.children?.map((child: any) => processNavigationItem(child)) || null,
+        onClick: navItem.children ? undefined : () => router.push(navItem._path),
+    };
+};
+
 const menuData: MenuBarOptions = computed(() => ({
     items: filteredNavigation.value?.map((navItem: any) => processNavigationItem(navItem)),
-    minWidth: 230,
 }));
 
 defineExpose({
